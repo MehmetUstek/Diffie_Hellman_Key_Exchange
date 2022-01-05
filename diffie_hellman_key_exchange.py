d63068804115323258ac466dbc45e98fd89fb89e
MehmetUstek <mustek17@ku.edu.tr>
2022-01-05 22:21:11 +0300

    Backup

--- diffie_hellman_key_exchange.py
+++ diffie_hellman_key_exchange.py
@@ -11,16 +11,22 @@ import threading
 prime_order_p = 16069
 generator_g = 21
 lock = threading.Lock()
+
+
 # semaphore = threading.Semaphore()
 
-
+# Given the generator g and prime order p, this function calculates the secret and
+# the public key of the user.
+# Namely, it randomly outputs a secret a, and calculates ga modulo p.
 def generate_secret_and_g_a(g, p):
     secret = np.random.randint(2, 20)
     # y = (g ** secret) % p
     y = pow(g, secret, p)
     return y, secret
 
-
+# Given the secret and the public key of the second party, the first party calculates its private key.
+# The following function for calculation is just like the one before,
+# ya modulo p = gab mod p.
 def calculate_private_key(y, secret, p):
     lock.acquire()
     # pk =  np.mod((np.power(y, secret)), p)
@@ -38,7 +44,11 @@ def write_to_file(message: str, filename):
 def append_fifteen_zeros_the_string(message: str):
     return message + "000000000000000"
 
-
+# Given the key, message and the filename, this function encrypts the message
+# using AES-128 CTR encryption, with a random nonce.
+# Then the function from Crypto.Cipher library encrypts the message and returns a
+# nonce and a ciphertext.
+# I wrote these two values to the file with comma separation.
 def encrypt(key, message: str, filename):
     data = bytes(message, encoding='utf-8')
     cipher = AES.new(key, AES.MODE_CTR)
@@ -51,21 +61,24 @@ def encrypt(key, message: str, filename):
     result = json.dumps({'nonce': nonce, 'ciphertext': ct})
     str = nonce + "," + ct_end_str
     write_to_file(str, filename)
-    print(result)
-    print(str)
+    # print(result)
+    # print(str)
 
     return result
 
 
 # decrypt
-def decrypt(json_input, key):
+#  Takes the message written to the file and splits it into nonce and ciphertext.
+#  Next, it decrypts the message using these two parameters with AES-128 CTR mode.
+#  It then returns the plaintext.
+def decrypt(message_input_from_file, key):
     try:
         # b64 = json.loads(json_input)
         # nonce = b64decode(b64['nonce'])
         # ct = b64decode(b64['ciphertext'])
-        json_input = json_input.split(",")
-        nonce = b64decode(json_input[0])
-        ct = b64decode(json_input[1][:-15])
+        message_input_from_file = message_input_from_file.split(",")
+        nonce = b64decode(message_input_from_file[0])
+        ct = b64decode(message_input_from_file[1][:-15])
 
         cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
         pt = cipher.decrypt(ct)
@@ -126,9 +139,6 @@ def user2(file: str):
     # semaphore.release()
 
 
-
-
-
 ## Encrypted communication phase
 
 # Unnecessary function.
@@ -138,13 +148,15 @@ def get_private_key(K_ab_A, K_ab_B):
         key = binascii.unhexlify(hashed_string)
         return key
 
-
+# K_ab_A is the calculated private key, namely the g^ab mod p.
+# This function returns the H(gab mod p) in an ascii form.
 def user1_key(K_ab_A):
     hashed_string = hl.sha256(str(K_ab_A).encode('utf-8')).hexdigest()
     key = binascii.unhexlify(hashed_string)
     return key
 
-
+# K_ab_B is the calculated private key, namely the g^ab mod p.
+# This function returns the H(gab mod p) in an ascii form.
 def user2_key(K_ab_B):
     hashed_string = hl.sha256(str(K_ab_B).encode('utf-8')).hexdigest()
     key = binascii.unhexlify(hashed_string)
@@ -164,6 +176,7 @@ def communication_phase(username1, username2, K_ab_A, K_ab_B, filename):
     user2Key = user2_key(K_ab_B)
     print(username2 + "'s key", user2Key)
     message_counter = 1
+    print("This conversation will go on until one of the parties input -1.")
     while input_from_user != "-1":
         input_from_user = input(username1 + "'s message:")
         if input_from_user == "-1":
@@ -175,7 +188,7 @@ def communication_phase(username1, username2, K_ab_A, K_ab_B, filename):
             # TODO: Change to 10.
             time.sleep(1)
         else:
-            print("current_message", encrypted_message)
+            # print("current_message", encrypted_message)
             decrypt(encrypted_message, user2Key)
         input_from_user = input(username2 + "'s message:")
         message_counter += 1
@@ -185,11 +198,10 @@ def communication_phase(username1, username2, K_ab_A, K_ab_B, filename):
             # TODO: Change to 10.
             time.sleep(1)
         else:
-            print("current_message", encrypted_message)
             decrypt(encrypted_message, user1Key)
 
 
-def attacker_communication_phase(username1, username2, K_ab_A1, K_ab_B1,K_ab_A2, K_ab_B2, file1, file2):
+def attacker_communication_phase(username1, username2, K_ab_A1, K_ab_B1, K_ab_A2, K_ab_B2, file1, file2):
     input_from_user = " "
     attackerKey_first_party = user1_key(K_ab_A)
     print("Attacker1's key", attackerKey_first_party)
@@ -200,11 +212,20 @@ def attacker_communication_phase(username1, username2, K_ab_A1, K_ab_B1,K_ab_A2,
     print("Attacker2's key", attackerKey_second_party)
     userKey_second_party = user2_key(K_ab_B)
     print(username2 + "'s key", userKey_second_party)
-
+    message_counter_1 = 1
     while input_from_user != "-1":
         input_from_user = input(username1 + "'s message:")
         if input_from_user == "-1":
             break
+        message_counter_1 += 1
+        dummy = user_send_message(input_from_user, userKey_first_party, file1)
+        encrypted_message = np.genfromtxt(file1, dtype="U")[message_counter_1]
+        if not encrypted_message:
+            # TODO: Change to 10.
+            time.sleep(1)
+        else:
+            # print("current_message", encrypted_message)
+            decrypt(encrypted_message, attackerKey_first_party)
         # First, the message is taken from first party. Then encrypted with first party key.
         encrypted_message = user_send_message(input_from_user, userKey_first_party, file1)
         # Then, the message is decrypted with attacker's key with first party.
@@ -230,9 +251,6 @@ def attacker_communication_phase(username1, username2, K_ab_A1, K_ab_B1,K_ab_A2,
         decrypt(encrypted_message, userKey_first_party)
 
 
-
-
-
 username1 = input("Please enter username for user 1" + "\n")
 t1 = threading.Thread(target=get_user_input, args=("Communication.txt",))
 t1.start()
@@ -243,6 +261,7 @@ t2.join()
 t1.join()
 print("First Part Communication Phase")
 communication_phase(username1, username2, K_ab_A, K_ab_B, "Communication.txt")
+
 
 # Unnecessary function
 # def copy_files_into_A_and_B(file1, file2):
@@ -289,8 +308,8 @@ def man_in_the_middle(file1, file2):
     second_party_K = np.copy(K_ab_B)
     # First with alice
     print("Man in the Middle Communication Phase")
-    attacker_communication_phase(username1, username2, attacker_K_with_first_party, first_party_K, attacker_K_with_second_party, second_party_K, file1,file2)
-
+    attacker_communication_phase(username1, username2, attacker_K_with_first_party, first_party_K,
+                                 attacker_K_with_second_party, second_party_K, file1, file2)
 
 
 file1 = "Communication_A.txt"
